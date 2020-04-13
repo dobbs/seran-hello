0dac678065de8288ba4ce81f0b78a3ce77491748
Eric Dobbs <eric@dobbse.net>
2020-04-13 08:43:36 -0600

    Rename crashes to plugin because the bug was fixed in seran-wiki

--- meta-sites/plugin.ts
+++ meta-sites/plugin.ts
@@ -2,8 +2,8 @@ import * as wiki from "seran/wiki.ts";
 
 export let plugins = ['/hello.mjs'];
 export let handler = new wiki.Handler();
-handler.page(wiki.welcomePage("[[DenoWiki]]", "[[Crashes]]"));
-handler.items("Crashes", () => [
-  wiki.item("hello", {text: "Hello, World! I'm a hello."})
+handler.page(wiki.welcomePage("[[DenoWiki]]", "[[Plugin]]"));
+handler.items("Plugin", () => [
+  wiki.item("hello", {text: "Hello, World! I'm a plugin."})
 ]);
 handler.plugins(import.meta.url, "client");
